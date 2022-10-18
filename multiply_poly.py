3e7a7110c8d3d845dd9d70eceefab8fd7d3f4a69
Reyce Salisbury <ras8734@rit.edu>
2022-10-18 16:29:32 -0400

    changed description of multipy_poly

--- multiply_poly.py
+++ multiply_poly.py
@@ -9,9 +9,9 @@ Argument 2 = poly_B
 
 Program operation:
 
-poly_A * poly_B % (0x1B)
+poly_A * poly_B % (0x11B)
 
-Note:   0x1B is the hexadecimal representation of the AES irr. polynomial, 
+Note:   0x11B is the hexadecimal representation of the AES irr. polynomial, 
         which is used to derive GF(2^8)
 """
 import sys
@@ -26,7 +26,7 @@ def main():
             sum = sum ^ hexpoly_A  # if last bit of b is 1, add poly_a to the sum
         hexpoly_B = hexpoly_B >> 1  # divide poly_B by 2
         hexpoly_A = hexpoly_A << 1  # multiply poly_A by 2
-        if (hexpoly_A & 0x100):
+        if (hexpoly_A & 0x100):  # this is 0x100 because this determines when overflow happens
             hexpoly_A = hexpoly_A ^ 0x11B  # mod poly_A by the AES irr. poly (0x11B)
 
     print(sum)
